824bea82b21f825a6592b55ff1697a743bbf8376
jonathanleivag <email@jonathanleivag.cl>
2022-12-07 21:49:27 -0300

    noti

--- App.tsx
+++ App.tsx
@@ -1,13 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import * as Notifications from 'expo-notifications'
+import * as Device from 'expo-device';
 
-export default function App() {
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true
+  })
+})
+
+export default function App () {
+  async function schedulePushNotification () {
+    if(Device.isDevice){
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Hola soy una notificación',
+          body: 'hola soy el cuerpo de la notificación',
+          data: { data: 'goes here' }
+        },
+        trigger: { seconds: 1 }
+      })
+    }else {
+      Alert.alert('Puta la wea xD No se puede enviar notificaciones en el emulador', 'Prueba en un dispositivo real', [{ text: 'Ok watsho' }])
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Enviar notificación'
+        onPress={async () => schedulePushNotification()}
+      />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +44,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
